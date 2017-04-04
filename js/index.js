2d5801909972e00e1fe1d6042ac15216fc171c07
Mariia Petryshyn <petryshyn.main@gmail.com>
2017-04-04 22:02:59 +0000

    js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-function sum(a, b, c, x) {
-    var result = a*(x*x) + b*x + c;
-    return result;
+var a = Number(prompt('write the number for "a"'));
+var b = Number(prompt('write the number for "b"'));
+var c = Number(prompt('write the number for "c"'));
+
+function quadraticEquation(a, b, c) {
+    var x1, x2;
+    var d = Math.pow(b,2) - 4*a*c;  
+    if (d > 0) {
+        var x1 = (-b + Math.sqrt(d))/(2*a);
+        var x2 = -b - Math.sqrt(d)/(2*a);
+    }
+       return x1 + 'x1', x2 + 'x2';
+    
+    if (d === 0) {
+        var x1 = -b/(2*a);
+        var x2 = -b/(2*a);
+        return 'x1=' + x1 , 'x2=' + x2;
+    }
+    
+    else (d<0) {
+        return 'Solution does not exist'
+    } 
+    
 }
-    var quadratic = sum(5, 23, 45, 3);
-    document.write(quadratic);
+    document.write(quadraticEquation(a, b, c));
